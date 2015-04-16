86be066ea36ead693b4ba600f7cfa6094d857e11
Kelly de Vries <kdv@me.com>
2015-04-15 20:58:46 -0700

    Add functioning html.

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,23 @@ var Ticket = {
 };
 
 
-// $(document).ready(function(){
-//   $()
-//
-// });
+$(document).ready(function(){
+  $('form#ticketmaster').submit(function(event){
+    event.preventDefault();
+
+    var vintage = $("select#vintage").val();
+    var age = $("select#age_range").val();
+    var showtime = $("select#showing").val();
+
+    var ticket = Object.create(Ticket);
+
+    ticket.old_release = vintage;
+    ticket.age_discount = age;
+    ticket.showing = showtime;
+
+    var newTicket = ticket.getPrice();
+
+    $('.ticket_price').text(newTicket);
+  });
+
+});
